868e687d713a47455e5053b0cc02b826b9814375
ponnanapavan <pavanponnana1@gmail.com>
2024-10-30 11:57:15 +0530

    socket cors change

--- jobportal-backend/server.js
+++ jobportal-backend/server.js
@@ -29,7 +29,7 @@ console.log(path.join(__dirname, '../jobportal-frontend/dist'))
 const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
-        origin: "http://localhost:5173",
+        origin: "http://localhost:5000",
     methods: ["GET", "POST"]
     }
 });
