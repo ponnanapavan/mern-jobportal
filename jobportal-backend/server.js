f12c05366e532b1271dd4d71c26c67c558e78bd8
ponnanapavan <pavanponnana1@gmail.com>
2024-10-30 11:52:46 +0530

    socket cors change

--- jobportal-backend/server.js
+++ jobportal-backend/server.js
@@ -29,7 +29,7 @@ console.log(path.join(__dirname, '../jobportal-frontend/dist'))
 const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
-        origin: "https://mern-jobportal-2.onrender.com",
+        origin: "*",
     methods: ["GET", "POST"]
     }
 });
